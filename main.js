7f6e76ff95b71db20e6dfb44f8904a3fd83aa3cb
Tazmondo <fondcoolman@outlook.com>
2021-07-28 18:34:56 +0100

    set min width to 600 (change in future)

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        minWidth: 600,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         },
