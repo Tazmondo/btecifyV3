f112acc5d12608f5d92c22d556e8c580dfdb3a62
Tazmondo <fondcoolman@outlook.com>
2021-07-31 18:40:48 +0100

    scrollbar nicer

--- renderer.js
+++ renderer.js
@@ -1,20 +1,5 @@
 console.log("renderer.js running...")
 
-
-
-function randomColourCSS() {
-    let r = Math.floor(Math.random()*256)
-    let g = Math.floor(Math.random()*256)
-    let b = Math.floor(Math.random()*256)
-    return `rgb(${r}, ${g}, ${b})`
-}
-
-window.api.receiveMoved(() => {
-    // [...document.querySelectorAll('*')].map((v) => {
-    //     v.style.backgroundColor = randomColourCSS()
-    // })
-})
-
 document.getElementById("shuffle").addEventListener("click", ev => {
     ev.currentTarget.classList.toggle("active")
 })
