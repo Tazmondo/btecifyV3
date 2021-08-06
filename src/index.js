7090a7fa0f7f4efa9f81f03799645d07baae1eb0
Tazmondo <fondcoolman@outlook.com>
2021-08-06 18:14:20 +0100

    refactor: routing moved to router.js

--- src/index.js
+++ src/index.js
@@ -1,56 +1,20 @@
-console.log("index.js running...")
+import {routeWithPageName} from "./router.js";
 
+//INITIALISE ROUTER
+import './router.js'
 // INITAIALISE PAGES
 import './homePage.js'
+
 import './playlistPage.js'
-
 // INITIALISE PLAYER
-import './footerPlayer.js'
 
+import './footerPlayer.js'
 // INITIALISE SO THAT RESULT IS CACHED
 import './objects.js'
 import './controller.js'
 
-function getPageIdFromNav(navName) {
-    return navName.slice(0, -4)
-}
+console.log("index.js running...")
 
-function routeWithPageName(pageName) {
-    let navId = pageName + "-nav"
-    let navElement = document.getElementById(navId)
-    routeWithNavElement(navElement)
-}
-
-function routeWithNavElement(navButton) {
-    navButton.classList.toggle("active", true)
-
-    let pageName = getPageIdFromNav(navButton.id)
-
-    let selectedPage = document.getElementById(navButton.id + "-page")
-    Array.from(document.querySelectorAll('main > div')).forEach(v => {
-        v.classList.toggle('switching', true)
-        v.classList.toggle('hidden', true)
-        setTimeout(() => {
-            v.classList.toggle('switching', false)
-        }, 700)
-    })
-    selectedPage.classList.toggle('hidden', false)
-
-    console.log(`Routed to ${pageName}.`)
-}
-
-let navButtons = Array.from(document.getElementById("nav-bar").childNodes).filter(v => {return v.nodeName === "BUTTON"})
-
-navButtons.forEach((v, i, arr) => {
-    v.addEventListener("click", e =>{
-        routeWithNavElement(v)
-        arr.forEach(v2 => {
-            if (v2 !== v) {
-                v2.classList.toggle("active", false)
-            }
-        })
-    })
-})
 
 // MAIN EXECUTION //
 
---
+++ src/router.js
@@ -0,0 +1,40 @@
+function getPageIdFromNavName(navName) {
+    return navName.slice(0, -4)
+}
+
+function routeWithNavElement(navButton) {
+    navButton.classList.toggle("active", true)
+
+    let pageName = getPageIdFromNavName(navButton.id)
+
+    let selectedPage = document.getElementById(navButton.id + "-page")
+    Array.from(document.querySelectorAll('main > div')).forEach(v => {
+        v.classList.toggle('switching', true)
+        v.classList.toggle('hidden', true)
+        setTimeout(() => {
+            v.classList.toggle('switching', false)
+        }, 700)
+    })
+    selectedPage.classList.toggle('hidden', false)
+
+    console.log(`Routed to ${pageName}.`)
+}
+
+let navButtons = Array.from(document.getElementById("nav-bar").childNodes).filter(v => {return v.nodeName === "BUTTON"})
+
+navButtons.forEach((v, i, arr) => {
+    v.addEventListener("click", e =>{
+        routeWithNavElement(v)
+        arr.forEach(v2 => {
+            if (v2 !== v) {
+                v2.classList.toggle("active", false)
+            }
+        })
+    })
+})
+
+export function routeWithPageName(pageName) {
+    let navId = pageName + "-nav"
+    let navElement = document.getElementById(navId)
+    routeWithNavElement(navElement)
+}
