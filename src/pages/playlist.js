6f8fc98cbc94379c89ee0615dfb0ebffbbb1da4b
Tazmondo <fondcoolman@outlook.com>
2021-08-11 21:55:29 +0100

    moved generatesongelement function to util.js

--- src/pages/playlist.js
+++ src/pages/playlist.js
@@ -1,4 +1,4 @@
-import {durationSecondsToMinutes, pageEntry, pageExit} from "../util.js";
+import {durationSecondsToMinutes, pageEntry, pageExit, generateSongElement} from "../util.js";
 
 import {EventController, MusicController, ObjectController} from '../controller.js'
 
@@ -49,107 +49,6 @@ function initPage() {
         newElement.innerText = title
 
         return newElement
-    }
-
-    function generateSongElement(song, playlist, superSub, otherPlaylist, isRightSide) {
-        let newSongItemContainer = document.createElement('div')
-        newSongItemContainer.classList.toggle("song-list-item-container")
-        if (superSub) {
-            newSongItemContainer.classList.toggle('super')
-        }
-
-        let template = document.querySelector('#song-list-item-template')
-
-        new IntersectionObserver((entries, observer) => {
-            entries.forEach( entry => {
-                if (entry.target.classList.contains('song-list-item-container')) {
-                    if (entry.isIntersecting) {
-                        if (!newSongItemContainer.classList.contains('seen')) {
-                            newSongItemContainer.classList.toggle("seen")
-                            populateSongItem()
-                        }
-                    } else {
-                        if (newSongItemContainer.classList.contains('seen')) {
-                            depopulateSongItem()
-                            newSongItemContainer.classList.toggle("seen")
-                        }
-                    }
-                }
-            })
-        }).observe(newSongItemContainer)
-
-        function depopulateSongItem() {
-            Array.from(newSongItemContainer.children).forEach(element => {  // Use Array.from since .children returns a live list
-                element.remove()
-            })
-        }
-
-        function populateSongItem() {
-            let newSongItem = template.content.firstElementChild.cloneNode(true)
-            newSongItem.classList.toggle('enabled', !song.isDisabled())
-
-            let thumb = newSongItem.querySelector('.thumb')
-            let title = newSongItem.querySelector('.title')
-            let artist = newSongItem.querySelector('.artist')
-            let album = newSongItem.querySelector('.album')
-            let duration = newSongItem.querySelector('.duration')
-
-            let removeButton = newSongItem.querySelector('.playlist-page-remove-from-playlist')
-
-
-            title.innerText = song.getTitle()
-            title.title = song.getTitle()
-            artist.innerText = song.getArtist()
-            album.innerText = song.getAlbum()
-            duration.innerText = durationSecondsToMinutes(song.getDurationSeconds())
-
-            removeButton.querySelector('title').textContent = `Remove from ${playlist.getTitle()}`
-            removeButton.addEventListener('click', e => {
-                removeFromPlaylist(playlist, song)
-            })
-
-            if (superSub) {
-                newSongItem.classList.toggle("super")
-
-                let addButtonEast = newSongItem.querySelector('.playlist-page-add-to-playlist-east')
-                let addButtonWest = newSongItem.querySelector('.playlist-page-add-to-playlist-west')
-                let addButton;
-
-                if (isRightSide) {
-                    addButtonWest.classList.toggle("hidden")
-                    addButton = addButtonWest
-                } else {
-                    addButtonEast.classList.toggle("hidden")
-                    addButton = addButtonEast
-                }
-
-                addButton.querySelector('title').textContent = `Add to ${otherPlaylist.getTitle()}`
-                addButton.addEventListener('click', e => {
-                    addToPlaylist(otherPlaylist, song)
-                })
-            }
-
-            let cachedThumb = song.getCachedThumb()
-            if (cachedThumb) {
-                thumb.style.backgroundImage = `url(${cachedThumb})`
-            } else {
-                song.getThumb().then(res => {
-                    thumb.style.backgroundImage = `url(${res})`
-                })
-            }
-
-
-            if (!song.isDisabled()) {
-                newSongItem.addEventListener('dblclick', e => {
-                    console.log(`Request play ${song.getTitle()}`);
-                    forceSetSong(song)
-                })
-            }
-
-            newSongItemContainer.insertAdjacentElement('afterbegin', newSongItem)
-        }
-
-        return newSongItemContainer
     }
 
     function playlistClickCallback(index, playlist, isSelected=false) {
