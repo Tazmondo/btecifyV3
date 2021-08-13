e0f792a2e34a323add1a1a11fe8dbb8f81b1837a
Tazmondo <fondcoolman@outlook.com>
2021-08-13 20:29:43 +0100

    basic song list done

--- src/pages/song.js
+++ src/pages/song.js
@@ -1,17 +1,36 @@
 import {pageEntry, pageExit} from "../util.js";
-
+import {EventController, ObjectController, util} from "../controller.js";
 
 function init() {
+    const {subscribe} = EventController
+    const {getAllSongs} = ObjectController
+    const {generateSongElement} = util
+
     let page = document.querySelector('#song-nav-page')
 
-    function drawPage() {
+    function undrawPage() {
+        let songList = page.querySelector('.song-list')
+        Array.from(songList.children).forEach(v => v.remove())
 
     }
 
-    drawPage()
+    function drawPage(songPlaylist) {
+        undrawPage()
+        if (songPlaylist) {
+            let songList = page.querySelector('.song-list')
+
+            let songArray = songPlaylist.getSongs()
+
+            songArray.forEach(song => songList.insertAdjacentElement('beforeend', generateSongElement(song)))
+
+        }
+    }
+
+    drawPage(getAllSongs())
+    subscribe('song', drawPage)
+
     pageEntry(page)
     return function unInitPage() {
-
         pageExit(page)
     }
 }
