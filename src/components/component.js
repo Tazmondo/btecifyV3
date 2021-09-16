d3e82a1727dbdde674c56b0c12419446491413b6
Tazmondo <fondcoolman@outlook.com>
2021-09-16 19:30:56 +0100

    prepare for component refactoring

---
+++ src/components/component.js
@@ -0,0 +1,8 @@
+import {generateList, generateQueue, generateCompare} from './song-list.js'
+
+function init() {
+
+    return {generateList, generateQueue, generateCompare}
+}
+
+export default init
---
+++ src/components/song-list.js
@@ -0,0 +1,28 @@
+function generateList(element, songs, ) {
+
+}
+
+function generateCompare() {
+
+}
+
+function generateQueue() {
+
+}
+
+/**
+ * Take an element and turn it into a songlist
+ * @param element {HTMLElement|Node} Must have class song-list
+ * @param songs {Song[]}
+ * @param playlist {Playlist}
+ * @param superSub {boolean}
+ * @param otherPlaylist {Playlist?}
+ * @param isRightSide {boolean}
+ * @param isPlayingSong {boolean}
+ * @param isHistorySong {boolean}
+ */
+function generateSongList(element, songs, playlist, superSub, otherPlaylist, isRightSide, isPlayingSong, isHistorySong, iobservedArray) {
+
+}
+
+export {generateList, generateQueue, generateCompare}
