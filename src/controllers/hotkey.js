62f1e5d0a93b0f119b47981704580334a6595af4
Tazmondo <fondcoolman@outlook.com>
2021-10-21 19:23:54 +0100

    hotkey.js refactored

--- src/controllers/hotkey.js
+++ src/controllers/hotkey.js
@@ -1,59 +1,66 @@
 import * as MusicController from './music.js'
 
-function init() {
-    const globalHotkeys = {
-        Pause:  ['F13', pause, false],
-        Skip: ['F14', skip, false],
-        'Increase Volume': ['F18', increaseVolume, false],
-        'Decrease Volume': ['F15', decreaseVolume, false]
-    }
+let globalHotkeys = {
+    Pause:  ['F13', pause, false],
+    Skip: ['F14', skip, false],
+    'Increase Volume': ['F18', increaseVolume, false],
+    'Decrease Volume': ['F15', decreaseVolume, false]
+}
 
-    const localHotkeys = { // obviously unfinished todo: finish local hotkeys
-        Pause:  ['space', pause, false],
-        Skip: ['right arrow', skip, false],
-        'Increase Volume': ['up arrow', increaseVolume, false],
-        'Decrease Volume': ['down arrow', decreaseVolume, false]
-    }
+let localHotkeys = { // obviously unfinished todo: finish local hotkeys
+    Pause:  ['space', pause, false],
+    Skip: ['right arrow', skip, false],
+    'Increase Volume': ['up arrow', increaseVolume, false],
+    'Decrease Volume': ['down arrow', decreaseVolume, false]
+}
 
-    registerHotkeys()
+function pause() {
+    MusicController.togglePlaying()
+}
 
-    function pause() {
-        MusicController.togglePlaying()
-    }
+function skip() {
+    MusicController.forward()
+}
 
-    function skip() {
-        MusicController.forward()
-    }
+function increaseVolume() {
+    MusicController.setVolume(MusicController.getInfo().volume + 0.02)
+}
 
-    function increaseVolume() {
-        MusicController.setVolume(MusicController.getInfo().volume + 0.02)
-    }
+function decreaseVolume() {
+    MusicController.setVolume(MusicController.getInfo().volume - 0.02)
+}
 
-    function decreaseVolume() {
-        MusicController.setVolume(MusicController.getInfo().volume - 0.02)
-    }
-
-    function registerHotkeys() {
-        api.unRegisterAllHotkeys()
-        for (let hotkey in globalHotkeys) {
-            globalHotkeys[hotkey][2] = api.registerHotkey(globalHotkeys[hotkey][0], globalHotkeys[hotkey][1])
-        }
-    }
-
-    return {
-        getHotKeys() {
-            return globalHotkeys
-        },
-
-        setHotkey(hotkeyName, hotkeyString) {
-            if (hotkeyName in globalHotkeys) {
-                globalHotkeys[hotkeyName][0] = hotkeyString
-                registerHotkeys()
-                return globalHotkeys[hotkeyName][2]
-            }
-            throw new Error("Tried to set an invalid hotkey!")
-        }
+function registerHotkeys() {
+    api.unRegisterAllHotkeys()
+    for (let hotkey in globalHotkeys) {
+        globalHotkeys[hotkey][2] = api.registerHotkey(globalHotkeys[hotkey][0], globalHotkeys[hotkey][1])
     }
 }
 
-export default init
+registerHotkeys()
+
+document.addEventListener('keypress', (e) => {
+    console.log(e.key);
+})
+
+function getHotKeys() {
+    return globalHotkeys
+}
+
+function setHotkey(hotkeyName, hotkeyString) {
+    if (hotkeyName in globalHotkeys) {
+        globalHotkeys[hotkeyName][0] = hotkeyString
+        registerHotkeys()
+        return globalHotkeys[hotkeyName][2]
+    }
+    throw new Error("Tried to set an invalid hotkey!")
+}
+
+function subscribeToKeydown(callback) {
+    let listener = (e) => {
+        e
+    }
+    document.addEventListener('keydown', (e) => {
+        console.log(e.key);
+    })
+}
