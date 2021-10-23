ce726034a3efacc998e029040dc12bfaeb9b7c9c
Tazmondo <fondcoolman@outlook.com>
2021-10-23 14:53:35 +0100

    added more options

--- src/controllers/options.js
+++ src/controllers/options.js
@@ -3,7 +3,39 @@ const options = {
         name: "Visualiser",
         type: "checkbox",
         value: true
+    },
+    visualiserDetail: {
+        name: "Visualiser Detail",
+        type: "number",
+        value: 8 // 2 to the power of value = fft size
+    },
+    downloadMusic: {
+        name: "Download music",
+        type: "checkbox",
+        value: true // todo: make me work
+    },
+    downloadThumbnails: {
+        name: "Download thumbnails",
+        type: "checkbox",
+        value: true // todo: make me work
+    },
+    youtubeKey: {
+        name: "Youtube API key",
+        type: "text",
+        value: ""
+    },
+    spotifyKey: {
+        name: "Spotify API key",
+        type: "text",
+        value: ""
+    },
+    volumeScale: {
+        name: "Volume Scale",
+        type: "number",
+        value: 2.5
     }
+
+    // colourTheme ?
 }
 
 function updateOption(optionKey, value) {
