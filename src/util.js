332b62c14339de9dd3646f440b763342156fc740
Tazmondo <fondcoolman@outlook.com>
2021-11-08 19:34:31 +0000

    add extract youtube id function

--- src/util.js
+++ src/util.js
@@ -137,3 +137,14 @@ export function makeDraggable(dragger, container) {
         }, {once: true})
     })
 }
+
+/**
+ * Given a youtube url, returns its id
+ * @param url {string}
+ * @return {string} Empty if no id, otherwise id
+ */
+export function extractId(url) {
+    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    let match = url.match(regExp);
+    return (match&&match[7].length===11)? match[7] : false;
+}
