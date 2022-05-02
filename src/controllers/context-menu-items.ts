e3a8c17691dfbbf30a41aa6ba36b4e66a64aa947
Tazmondo <fondcoolman@outlook.com>
2022-05-02 03:18:10 +0100

    Typescriptify context menu items.js

--- src/controllers/context-menu-items.ts
+++ src/controllers/context-menu-items.ts
@@ -3,12 +3,13 @@ import * as ObjectController from './object.js'
 import {deleteSong, deleteUnusedSongs, getSongFromId} from './object.js'
 import * as RouteController from './route.js'
 import {generateInputDialog} from "./dialog-box.js";
+import {apiPlaylistShallow} from "./types";
 
-function getOtherPlaylists(context, callback) {
-    let newItems = []
-    ObjectController.getPlaylistArray().filter(v => v.getTitle() !== context.querySelector('h3').textContent).forEach(playlist => {
+function getOtherPlaylists(context: HTMLElement, callback: (context: HTMLElement, playlist: apiPlaylistShallow) => void) {
+    let newItems: ContextItem[] = []
+    ObjectController.getPlaylistArray().filter(v => v.title !== context.querySelector('h3')?.textContent).forEach(playlist => {
         newItems.push({
-            name: playlist.getTitle(),
+            name: playlist.title,
             type: 'button',
             action: () => {
                 callback(context, playlist)
@@ -18,7 +19,15 @@ function getOtherPlaylists(context, callback) {
     return [{context, actions: newItems}]
 }
 
-const contexts = {
+interface ContextItem {
+    name: string
+    type: "button" | "extend"
+    action: (context: any, extensionSelection: any) => void
+    extensionItems?: (context: HTMLElement, callback: (arg0: any) => void) => { context: HTMLElement, actions: ContextItem[] }[]
+    disabled?: (context: HTMLElement) => boolean
+}
+
+const contexts: { [key: string]: ContextItem[] } = {
     'body': [
         // {
         //     name: 'Test Dialog box',
@@ -49,22 +58,23 @@ const contexts = {
                             label: "Playlist URL (optional)"
                         }
                     ]
-                }).then(res => {
-                    let name = res[0]
-                    let url = res[1]
-                    if (typeof name === "string" && name !== "") {
-                        if (typeof url === "string" && url !== "") {
+                }).then(async res => {
+                    if (!(typeof res == "boolean")) {
+                        let name = res[0]
+                        let url = res[1]
+                        if (name !== "" && url !== "") {
                             ObjectController.makeRemotePlaylist(name, url).then(newPlaylist => {
                                 if (newPlaylist) {
                                     RouteController.baseRoute('playlistView', [newPlaylist])
                                 }
                             })
-                        } else{
-                            let newPlaylist = ObjectController.makePlaylist(name)
+                        } else {
+                            let newPlaylist = await ObjectController.makePlaylist(name)
                             if (newPlaylist) {
                                 // RouteController.baseRoute('playlistView', [newPlaylist])
                             }
                         }
+
                     }
                 })
             }
@@ -75,21 +85,23 @@ const contexts = {
         {
             name: 'Play',
             type: 'button',
-            action: (context) => {
+            action: (context: HTMLElement) => {
                 // Not sure if context should just be the element or whether it
                 // should just pass the relevant details, like playlist title
-                MusicController.setPlaylist(
-                    ObjectController.getPlaylistFromTitle(
-                        context.querySelector('h3').textContent
-                    )
-                )
+                let title = context.querySelector('h3')?.textContent;
+                if (title) {
+                    let playlistFromTitle = ObjectController.getPlaylistFromTitle(title);
+                    if (playlistFromTitle) {
+                        MusicController.setPlaylist(playlistFromTitle)
+                    }
+                }
             }
         },
         {
             name: 'View songs',
             type: 'button',
-            action: context => {
-                let title = context.querySelector('h3').textContent;
+            action: (context: HTMLElement) => {
+                let title = context.querySelector('h3')!.textContent!
                 let playlist = ObjectController.getPlaylistFromTitle(title)
 
                 RouteController.baseRoute('playlistView', [playlist])
@@ -98,9 +110,9 @@ const contexts = {
         {
             name: 'Compare with...',
             type: 'extend',
-            action: (context, extensionSelection) => {
-                let title1 = context.querySelector('h3').textContent;
-                let title2 = extensionSelection?.getTitle();
+            action: (context: HTMLElement, extensionSelection: apiPlaylistShallow) => {
+                let title1 = context.querySelector('h3')?.textContent
+                let title2 = extensionSelection.title;
                 RouteController.baseRoute('playlist', [title1, title2])
             },
             extensionItems: getOtherPlaylists
@@ -108,27 +120,26 @@ const contexts = {
         {
             name: 'Merge songs from...',
             type: 'extend',
-            action: (context, extensionSelection) => {
-                let title1 = context.querySelector('h3').textContent
-                let title2 = extensionSelection?.getTitle()
+            action: (context: HTMLElement, extensionSelection: apiPlaylistShallow) => {
+                let title1 = context.querySelector('h3')!.textContent
+                let title2 = extensionSelection?.title
                 // todo: add merging implementation
             },
             extensionItems: getOtherPlaylists
         },
         {
-
-        },
-        {
             name: 'Rename Playlist',
             type: 'button',
-            action: context => {
-                let playlist = ObjectController.getPlaylistFromTitle(context.querySelector('h3').textContent)
+            action: (context: HTMLElement) => {
+                let playlist = ObjectController.getPlaylistFromTitle(context.querySelector('h3')?.textContent!)!
                 generateInputDialog("Rename", `Rename ${playlist.getTitle()}...`, {
                     inputs: [{type: 'text', label: 'New Title'}]
                 }).then(res => {
-                    let newTitle = res[0]
-                    if (newTitle && typeof newTitle === "string" && newTitle !== playlist.getTitle()) {
-                        ObjectController.renamePlaylist(playlist, newTitle)
+                    if (!(typeof res == "boolean")) {
+                        let newTitle = res[0]
+                        if (newTitle !== undefined && newTitle !== playlist.getTitle()) {
+                            ObjectController.renamePlaylist(playlist, newTitle)
+                        }
                     }
                 })
                 // rename playlist
@@ -137,23 +148,23 @@ const contexts = {
         { // todo
             name: 'Set Thumbnail',
             type: 'button',
-            action: context => {
+            action: (context: HTMLElement) => {
                 // set new thumbnail
             }
         },
         {
             name: 'Refresh Thumbnail',
             type: 'button',
-            action: context => {
-                let playlist = ObjectController.getPlaylistFromTitle(context.querySelector('h3').textContent)
+            action: (context: HTMLElement) => {
+                let playlist = ObjectController.getPlaylistFromTitle(context.querySelector('h3')?.textContent!)!
                 playlist.refreshThumb()
             }
         },
         {
             name: 'Delete Playlist',
             type: 'button',
-            action: (context) => {
-                let playlist = ObjectController.getPlaylistFromTitle(context.querySelector('h3').textContent)
+            action: (context: HTMLElement) => {
+                let playlist = ObjectController.getPlaylistFromTitle(context.querySelector('h3')?.textContent!)!
                 let res = ObjectController.deletePlaylist(playlist)
             }
         }
@@ -162,7 +173,7 @@ const contexts = {
         {
             name: 'Delete unused songs',
             type: 'button',
-            action: context => {
+            action: () => {
                 deleteUnusedSongs()
             }
         }
@@ -172,28 +183,28 @@ const contexts = {
         {
             name: 'Play',
             type: 'button',
-            action: (context) => {
+            action: (context: HTMLElement) => {
                 context.dispatchEvent(new Event('dblclick'))
             }
         },
         {
             name: 'Delete',
             type: 'button',
-            action: (context) => {
-                let uuid = context?.dataset?.uuid
-                if (uuid) {
-                    deleteSong(getSongFromId(uuid))
+            action: (context: HTMLElement) => {
+                let id = context?.dataset?.uuid
+                if (id) {
+                    deleteSong(getSongFromId(Number(id))!)
                 }
             }
         },
         {
             name: 'Copy URL to clipboard',
             type: 'button',
-            action: context => {
+            action: (context: HTMLElement) => {
                 let uuid = context?.dataset?.uuid
                 if (uuid) {
-                    let song = getSongFromId(uuid)
-                    navigator.clipboard.writeText(song.getURL())
+                    let song = getSongFromId(Number(uuid))!
+                    navigator.clipboard.writeText(song.weburl)
                 }
             }
         }
@@ -203,11 +214,11 @@ const contexts = {
         {
             name: 'Add to other playlist',
             type: 'button',
-            disabled: (context) => {
-                return context.querySelectorAll('.playlist-page-add-to-playlist.inactive').length === 2
+            disabled: (context: HTMLElement) => {
+                return context.querySelectorAll('.playlist-page-add-to-playlist.inactive')?.length === 2
             },
-            action: (context) => {
-                context.querySelector('.playlist-page-add-to-playlist').dispatchEvent(new Event('click'))
+            action: (context: HTMLElement) => {
+                context.querySelector('.playlist-page-add-to-playlist')!.dispatchEvent(new Event('click'))
             }
         }
     ],
@@ -216,8 +227,8 @@ const contexts = {
         {
             name: 'Remove from this playlist',
             type: 'button',
-            action: (context) => {
-                context.querySelector('.playlist-page-remove-from-playlist').dispatchEvent(new Event('click'))
+            action: (context: HTMLElement) => {
+                context.querySelector('.playlist-page-remove-from-playlist')!.dispatchEvent(new Event('click'))
             }
         },
     ],
@@ -226,23 +237,25 @@ const contexts = {
         {
             name: 'Copy image',
             type: 'button',
-            action: (context) => {
+            action: (context: HTMLImageElement) => {
                 let newImg = new Image() // Since the og image object may be cropped or resized etc
                 let backgroundImage = context.style.backgroundImage;
 
                 // Image is either a src or a background image.
-                newImg.src = context.src || (backgroundImage.substring(5, backgroundImage.length - 2))
-                let canvas = document.createElement('canvas');
-                let canvasContext = canvas.getContext('2d');
+                newImg.src = (context as HTMLImageElement).src || (backgroundImage.substring(5, backgroundImage.length - 2))
+                let canvas = document.createElement('canvas')
+                let canvasContext = canvas.getContext('2d')!
                 canvas.width = newImg.width;
                 canvas.height = newImg.height;
                 canvasContext.drawImage(newImg, 0, 0);
                 canvas.toBlob(blob => {
-                    navigator.clipboard.write(
-                        [new ClipboardItem({
-                            [blob.type]: blob
-                        })]
-                    )
+                    if (blob !== null) {
+                        navigator.clipboard.write(
+                            [new ClipboardItem({
+                                [blob.type]: blob
+                            })]
+                        )
+                    }
                 })
             }
         }
